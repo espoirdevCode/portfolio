4f17b21d5fd0d94f0bd1d4367e3682dde0434e19
TheLeader-Community <TheLeaderOfCommunity@gmail.com>
2025-11-16 07:58:06 +0100

    remove cv

--- app/(pages)/cv/page.tsx
+++ app/(pages)/cv/page.tsx
@@ -4,7 +4,8 @@ export default function page() {
     return (
         <div>
             <Header />
-            <iframe src="/pdf/cv_chadrack_massamba_fr.pdf" className=" w-full h-screen my-20 bg-black" ></iframe>
+            <p>ok ici</p>
+            {/* <iframe src="/pdf/cv_chadrack_massamba_fr.pdf" className=" w-full h-screen my-20 bg-black" ></iframe> */}
         </div>
     )
 }
