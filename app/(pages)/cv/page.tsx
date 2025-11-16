3e6d3f68c513dc76e6599bc1e7279a856730a562
TheLeader-Community <TheLeaderOfCommunity@gmail.com>
2025-11-16 08:03:36 +0100

    update cv

--- app/(pages)/cv/page.tsx
+++ app/(pages)/cv/page.tsx
@@ -4,8 +4,7 @@ export default function page() {
     return (
         <div>
             <Header />
-            <p>ok ici</p>
-            {/* <iframe src="/pdf/cv_chadrack_massamba_fr.pdf" className=" w-full h-screen my-20 bg-black" ></iframe> */}
+            <iframe src="/pdf/cv_chadrack_massamba_fr.pdf" className=" w-full h-screen my-20 bg-black" ></iframe>
         </div>
     )
 }
